d8ab61bc278f11a28442aaedc39dac21727821bf
lch9527 <38733164+lch9527@users.noreply.github.com>
2023-04-28 13:35:08 -0700

    add c++ class

---
+++ Source/VRExpPluginExample/Cpp_Harvester.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Misc/VRWheeledVehicle.h"
+#include "Cpp_Harvester.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VREXPPLUGINEXAMPLE_API ACpp_Harvester : public AVRWheeledVehicle
+{
+	GENERATED_BODY()
+	
+};
